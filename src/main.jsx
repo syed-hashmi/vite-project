a2e9de2e3cd4daad372049754241f6776ab79d6f
Syed Sarosh Hashmi <hashmisarosh@gmail.com>
2025-10-15 03:35:46 +0400

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { createContext, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router'
-import Dashboard from './components/dashboard/Dashboard.jsx'
 import Welcome from './components/welcome/welcome.jsx'
 import Login from './components/login/login.jsx'
 import { store } from '../util/createStore.jsx'
 import { Provider } from 'react-redux'
 import Home from './components/home/Home.jsx'
-import { useContext } from 'react'
+import Dashboard from './components/dashboard/Dashboard.jsx'
 
 
 export let myValue = createContext("hello  world from context api value");
